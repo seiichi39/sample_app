23f7cca4a8f23174da1acb89d344df17c74a6cb5
seiichi39 <jhnnw708@gmail.com>
2022-10-11 11:08:19 +0900

    “step1-3”

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  # deviseコントローラーにストロングパラメータを追加する          
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # サインアップ時にnameとphone_numberのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,19 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['MAIL'],
+    :password => ENV['MAIL_PASSWORD'],
+    :authentication => :plain
+  }  
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
